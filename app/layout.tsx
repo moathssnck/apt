f425324b879b63fe024f3e3aa47e1bd487be14a1
moathssnck <myneww00@gmail.com>
2025-12-03 14:21:30 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
         {/* Google Tag Script */}
         <Script
           async
-          src="https://www.googletagmanager.com/gtag/js?id=AW-17766162614"
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17776840689"
         />
 
         <Script id="google-analytics">
@@ -41,7 +41,7 @@ export default function RootLayout({
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'AW-17766162614');
+            gtag('config', 'AW-17776840689');
           `}
         </Script>
       </body>
